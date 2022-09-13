d9428d397e034c59616a6fb86eb0e06e9d2b02a6
Svetlana <svgf@mail.ru>
2022-09-13 15:13:02 +0300

    Задача 023 работает

---
+++ Exmpl023/Program.cs
@@ -0,0 +1,20 @@
+﻿
+// Задача 23
+// Напишите программу, которая принимает на вход число (N)
+// и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+Console.Clear();
+Console.WriteLine("Задача 23: Создадим таблицу кубов чисел от 1 до N.");
+int N;
+double Cubes;
+Console.Write("Введите значение N: ");
+N = Convert.ToInt32(Console.ReadLine());
+
+for (int i=1;i<N+1;i++)
+{
+Cubes = Math.Pow(i, 3);
+Console.WriteLine($"{i} ^ 3 = {Cubes}");
+//Console.WriteLine();
+}
